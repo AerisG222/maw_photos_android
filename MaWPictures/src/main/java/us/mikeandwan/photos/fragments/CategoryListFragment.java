10febef43d1753e853c078bea38c9b12dfb34c75
Mike Morano <mmorano@sephiroth.mikeandwan.us>
2017-01-29 14:41:47 -0500

    remove unused imports

--- MaWPictures/src/main/java/us/mikeandwan/photos/fragments/CategoryListFragment.java
+++ MaWPictures/src/main/java/us/mikeandwan/photos/fragments/CategoryListFragment.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 
 import java.util.List;
 
@@ -16,7 +15,6 @@ import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnItemSelected;
 import butterknife.Unbinder;
 import us.mikeandwan.photos.R;
 import us.mikeandwan.photos.di.TaskComponent;
