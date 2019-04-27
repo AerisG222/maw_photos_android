6afd853739d6f7468ca4b941db0536f9b6d3db85
Mike Morano <mmorano@mikeandwan.us>
2019-04-27 12:40:48 -0400

    initialize an empty list to avoid potential null refs

--- MaWPhotos/src/main/java/us/mikeandwan/photos/models/ApiCollection.java
+++ MaWPhotos/src/main/java/us/mikeandwan/photos/models/ApiCollection.java
@@ -1,10 +1,11 @@
 package us.mikeandwan.photos.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ApiCollection<T> {
     private long _count;
-    private List<T> _items;
+    private List<T> _items = new ArrayList<>();
 
     public long getCount() { return _count; }
     public void setCount(long count) { _count = count; }
