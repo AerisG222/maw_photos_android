9e34afaa678ef4f1e71fb2b121dceca72a5f2a9c
Mike Morano <mmorano@mikeandwan.us>
2021-09-13 10:38:57 -0400

    convert to kotlin coroutine

--- MaWPhotos/src/main/java/us/mikeandwan/photos/services/AuthAuthenticator.kt
+++ MaWPhotos/src/main/java/us/mikeandwan/photos/services/AuthAuthenticator.kt
@@ -1,5 +1,6 @@
 package us.mikeandwan.photos.services
 
+import kotlinx.coroutines.*
 import net.openid.appauth.AuthorizationException
 import net.openid.appauth.AuthorizationService
 import okhttp3.Authenticator
@@ -8,7 +9,6 @@ import okhttp3.Response
 import okhttp3.Route
 import timber.log.Timber
 import java.io.IOException
-import java.util.concurrent.CompletableFuture
 
 class AuthAuthenticator(
     private val _authService: AuthorizationService,
@@ -17,32 +17,33 @@ class AuthAuthenticator(
     @Synchronized
     @Throws(IOException::class)
     override fun authenticate(route: Route?, response: Response): Request? {
-        val future = CompletableFuture<Request?>()
         val authState = _authStateManager.current
+        var request: Request? = null
 
         Timber.d("Starting Authenticator.authenticate")
 
-        authState.performActionWithFreshTokens(_authService) { accessToken: String?, idToken: String?, ex: AuthorizationException? ->
-            if (ex != null) {
-                Timber.e("Failed to authorize = %s", ex.message)
-                future.complete(null)
-            } else if (accessToken == null) {
-                Timber.e("Failed to authorize, received null access token")
-                future.complete(null) // Give up, we've already failed to authenticate.
-            } else {
-                Timber.i("authenticate: obtained access token")
-                val request = response.request().newBuilder()
-                    .header("Authorization", String.format("Bearer %s", accessToken))
-                    .build()
-                future.complete(request)
+        runBlocking {
+            launch {
+                authState.performActionWithFreshTokens(_authService) { accessToken: String?, idToken: String?, ex: AuthorizationException? ->
+                    if (ex != null) {
+                        Timber.e("Failed to authorize = %s", ex.message)
+
+                        request = null
+                    } else if (accessToken == null) {
+                        Timber.e("Failed to authorize, received null access token")
+
+                        request = null // Give up, we've already failed to authenticate.
+                    } else {
+                        Timber.i("authenticate: obtained access token")
+
+                        request = response.request().newBuilder()
+                            .header("Authorization", String.format("Bearer %s", accessToken))
+                            .build()
+                    }
+                }
             }
         }
 
-        return try {
-            future.get()
-        } catch (ex: Exception) {
-            Timber.e("Error: %s", ex.message)
-            null
-        }
+        return request
     }
 }
