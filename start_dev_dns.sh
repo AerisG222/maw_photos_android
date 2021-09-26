df6243dc43bc5d14ffb16fbd9bf0777623ba4847
Mike Morano <mmorano@mikeandwan.us>
2021-09-26 07:00:54 -0400

    updated scripts to now use dnsmasq

--- start_dev_dns.sh
+++ start_dev_dns.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo '*** Starting Dev DNS ***'
-dnsmasq --no-hosts \
-        --listen-address 127.0.0.1 \
-        --port 5300 \
-        --keep-in-foreground \
-        --log-queries \
-        --address /mikeandwan.us/10.0.2.2
+sudo dnsmasq \
+    --no-hosts \
+    --listen-address 127.0.0.1 \
+    --keep-in-foreground \
+    --log-queries \
+    --address /mikeandwan.us/10.0.2.2
--- start_dev_emulator.sh
+++ start_dev_emulator.sh
@@ -4,33 +4,12 @@ EMU="${SDK}/emulator/emulator"
 ADB="${SDK}/platform-tools/adb"
 
 echo '*** Starting Emulator ***'
-$EMU -avd Pixel_4a_API_30 -no-snapshot -dns-server 192.168.1.1 &
+$EMU -avd Pixel_4a_API_30 -no-snapshot -dns-server 127.0.0.1 &
 
-#echo '*** Wait for Emulator to Start ***'
+echo '*** Wait for Emulator to Start ***'
 sleep 20
 
-#echo '*** Mount Emulator ***'
-#$ADB root
-#$ADB remount
-#while [ $? -ne 0 ]
-#do
-#   sleep 1
-#   echo "  - retrying adb remount..."
-#   $ADB remount
-#done
-
-#echo '*** Hosts File ***'
-#$ADB push ~/git/maw_photos_android/dev_etc_hosts /system/etc/hosts
-
-#if [ $? -ne 0 ]
-#then
-# echo "  - failed to push hosts file to android device"
-#else
-# echo "  - hosts file pushed successfully"
-#fi
-
 echo '*** Certificate Authority ***'
-
 echo '  - copying public cert to emulator'
 $ADB push ~/git/mikeandwan.us/maw_certs/ca/ca.crt /sdcard/mawdev.public.crt
 
