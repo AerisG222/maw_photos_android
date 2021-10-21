5ee213c6b41f0784aab38ed78bd0b5725d577235
Mike Morano <mmorano@mikeandwan.us>
2021-10-21 07:00:17 -0400

    close drawer after selecting random menu item

--- MaWPhotos/src/main/java/us/mikeandwan/photos/uinew/ui/randomnavmenu/RandomMenuViewModel.kt
+++ MaWPhotos/src/main/java/us/mikeandwan/photos/uinew/ui/randomnavmenu/RandomMenuViewModel.kt
@@ -5,21 +5,27 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import us.mikeandwan.photos.domain.ActiveIdRepository
+import us.mikeandwan.photos.domain.NavigationStateRepository
 import us.mikeandwan.photos.domain.PhotoCategoryRepository
 import us.mikeandwan.photos.domain.RandomPhotoRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class RandomMenuViewModel @Inject constructor (
-    private val randomPhotoRepository: RandomPhotoRepository
+    private val randomPhotoRepository: RandomPhotoRepository,
+    private val navigationStateRepository: NavigationStateRepository
 ): ViewModel() {
     fun fetch(count: Int) {
         viewModelScope.launch {
             randomPhotoRepository.fetch(count)
         }
+
+        navigationStateRepository.requestNavDrawerClose()
     }
 
     fun clear() {
         randomPhotoRepository.clear()
+
+        navigationStateRepository.requestNavDrawerClose()
     }
 }
