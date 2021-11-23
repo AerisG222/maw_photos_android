4853f03d777592494dacae816324e266520684dc
Mike Morano <mmorano@mikeandwan.us>
2021-11-22 21:13:42 -0500

    add a comment / note about some issues in local dev testing

--- MaWPhotos/src/main/java/us/mikeandwan/photos/di/NetworkModule.kt
+++ MaWPhotos/src/main/java/us/mikeandwan/photos/di/NetworkModule.kt
@@ -7,6 +7,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.Protocol
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
 import us.mikeandwan.photos.Constants
@@ -24,7 +25,12 @@ class NetworkModule {
         authenticator: AuthAuthenticator,
         authInterceptor: AuthInterceptor
     ): OkHttpClient {
+        // NOTE: the protocols line was useful when testing locally, but ideally should not be used in prod
+        // this fixed an error testing locally where kestrel would reset the http2 connection when access token expired.
+        // this resulted in OkHttp bailing and not trying the authenticator to get a new token.  changing to 1.1 seemed
+        // to work better and avoid this issue when testing locally.
         return OkHttpClient.Builder()
+            //.protocols(listOf(Protocol.HTTP_1_1))
             .authenticator(authenticator)
             .addInterceptor(authInterceptor)
             .build()
