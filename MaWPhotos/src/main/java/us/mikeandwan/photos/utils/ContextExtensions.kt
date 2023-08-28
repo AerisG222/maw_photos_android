f0e4fc7ebdc455f10ad12f5d000693fc9ac1389b
Mike Morano <mmorano@mikeandwan.us>
2023-08-28 16:19:19 -0400

    not sure why this reference worked yesterday, but this alternative seems to work fine too

--- MaWPhotos/src/main/java/us/mikeandwan/photos/utils/ContextExtensions.kt
+++ MaWPhotos/src/main/java/us/mikeandwan/photos/utils/ContextExtensions.kt
@@ -3,10 +3,9 @@ package us.mikeandwan.photos.utils
 import android.content.Context
 import android.content.res.Resources
 import android.util.TypedValue
-import us.mikeandwan.photos.R
 
 fun Context.getTextColor(isActive: Boolean): Int {
-    val colorAttribute = if(isActive) R.attr.colorPrimary else R.attr.colorOnSurface
+    val colorAttribute = if(isActive) com.google.android.material.R.attr.colorPrimary else com.google.android.material.R.attr.colorOnSurface
 
     return getColorFromAttribute(colorAttribute)
 }
