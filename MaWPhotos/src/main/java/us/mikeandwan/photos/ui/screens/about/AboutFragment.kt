414701ceecd139bbaadba3049aa80a9261742ac9
Mike Morano <mmorano@mikeandwan.us>
2024-03-17 15:18:24 -0400

    migrate about screen to compose

--- MaWPhotos/src/main/java/us/mikeandwan/photos/ui/screens/about/AboutFragment.kt
+++ MaWPhotos/src/main/java/us/mikeandwan/photos/ui/screens/about/AboutFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -14,15 +16,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import us.mikeandwan.photos.R
-import us.mikeandwan.photos.databinding.FragmentAboutBinding
 
 @AndroidEntryPoint
 class AboutFragment : Fragment() {
-    companion object {
-        fun newInstance() = AboutFragment()
-    }
-
-    private lateinit var binding: FragmentAboutBinding
     private val viewModel by viewModels<AboutViewModel>()
 
     override fun onCreateView(
@@ -30,13 +26,14 @@ class AboutFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentAboutBinding.inflate(inflater)
-        binding.lifecycleOwner = viewLifecycleOwner
-        binding.viewModel = viewModel
-
         initStateObservers()
 
-        return binding.root
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                AboutScreen(viewModel)
+            }
+        }
     }
 
     private fun initStateObservers() {
---
+++ MaWPhotos/src/main/java/us/mikeandwan/photos/ui/screens/about/AboutScreen.kt
@@ -0,0 +1,88 @@
+package us.mikeandwan.photos.ui.screens.about
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import us.mikeandwan.photos.R
+
+@Composable
+fun AboutScreen(
+    viewModel: AboutViewModel
+) {
+    val tangerine = FontFamily(Font(R.font.tangerine))
+    val history = viewModel.history.collectAsState()
+
+    Column {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            AsyncImage(
+                model = R.drawable.ic_launch,
+                contentDescription = stringResource(id = R.string.logo_description),
+                modifier = Modifier.size(96.dp)
+            )
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "mikeandwan.us",
+                fontSize = 42.sp,
+                fontFamily = tangerine,
+            )
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Photos",
+                fontSize = 42.sp,
+                fontFamily = tangerine,
+            )
+        }
+        Row(
+            modifier = Modifier
+                .padding(0.dp, 0.dp, 0.dp, 16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = viewModel.version
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text(
+                text = history.value,
+            )
+        }
+    }
+}
