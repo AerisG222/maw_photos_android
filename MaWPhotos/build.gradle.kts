5aedf10de423f65cabec5d5882fd1dbfa2cf0e9f
Mike Morano <mmorano@mikeandwan.us>
2024-03-23 08:20:25 -0400

    use java 17

--- MaWPhotos/build.gradle.kts
+++ MaWPhotos/build.gradle.kts
@@ -44,8 +44,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     composeOptions {
@@ -53,7 +53,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
 
         freeCompilerArgs = listOf(
                 "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
