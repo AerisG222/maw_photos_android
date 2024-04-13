55fb9b7ca666c2816f888b72d1d9585b4f21e44e
Mike Morano <mmorano@mikeandwan.us>
2024-04-13 19:27:03 -0400

    remove the title from the top of the screen

--- MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/main/MainActivity.kt
+++ MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package us.mikeandwan.photos.ui.main
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,6 +12,8 @@ import us.mikeandwan.photos.ui.theme.AppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContent {
             AppTheme {
