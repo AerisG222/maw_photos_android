b74557c7384ea6541294138f66095ee133223611
Mike Morano <mmorano@mikeandwan.us>
2024-04-26 18:55:50 -0400

    improve styling of search nav menu

--- MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/controls/searchnavmenu/SearchListItem.kt
+++ MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/controls/searchnavmenu/SearchListItem.kt
@@ -3,6 +3,7 @@ package us.mikeandwan.photos.ui.controls.searchnavmenu
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -22,7 +23,10 @@ fun SearchListItem(
             onClick = { onTermSelected(term) },
             modifier = modifier.fillMaxWidth()
         ) {
-            Text(text = term)
+            Text(
+                text = term,
+                color = MaterialTheme.colorScheme.primary
+            )
         }
     }
 }
--- MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/controls/searchnavmenu/SearchListMenu.kt
+++ MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/controls/searchnavmenu/SearchListMenu.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -38,6 +39,7 @@ fun SearchListMenu(
             ) {
                 Text(
                     text = "No recent searches",
+                    color = MaterialTheme.colorScheme.onSurface,
                     fontStyle = FontStyle.Italic
                 )
             }
