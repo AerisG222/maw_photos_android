918e654ff8f6a1df763279a41975c1257dc8e614
Mike Morano <mmorano@mikeandwan.us>
2024-05-04 07:51:22 -0400

    some styling improvements

--- MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/controls/photodetail/PhotoDetailTabs.kt
+++ MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/controls/photodetail/PhotoDetailTabs.kt
@@ -3,17 +3,16 @@ package us.mikeandwan.photos.ui.controls.photodetail
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import kotlinx.coroutines.launch
 import us.mikeandwan.photos.R
@@ -24,7 +23,6 @@ import us.mikeandwan.photos.ui.controls.photorating.PhotoRatingScreen
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun PhotoDetailTabs() {
-    var state by remember { mutableStateOf(0) }
     val pagerState = rememberPagerState(pageCount = { 3 })
     val coroutineScope = rememberCoroutineScope()
 
@@ -33,49 +31,52 @@ fun PhotoDetailTabs() {
     val IDX_EXIF = 2
 
     Column(modifier = Modifier.fillMaxSize()) {
-        TabRow(selectedTabIndex = state) {
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            contentColor = MaterialTheme.colorScheme.onSurface
+        ) {
             Tab(
-                selected = state == IDX_RATING,
+                selected = pagerState.currentPage == IDX_RATING,
                 onClick = {
-                    state = IDX_RATING
                     coroutineScope.launch {
-                        pagerState.scrollToPage(state)
+                        pagerState.animateScrollToPage(IDX_RATING)
                     }
                 },
                 icon = {
                     AsyncImage(
                         model = R.drawable.ic_star,
-                        contentDescription = "Ratings"
+                        contentDescription = "Ratings",
+                        modifier = Modifier.size(32.dp)
                     )
-                }
+                },
             )
             Tab(
-                selected = state == IDX_COMMENT,
+                selected = pagerState.currentPage == IDX_COMMENT,
                 onClick = {
-                    state = IDX_COMMENT
                     coroutineScope.launch {
-                        pagerState.scrollToPage(state)
+                        pagerState.animateScrollToPage(IDX_COMMENT)
                     }
                 },
                 icon = {
                     AsyncImage(
                         model = R.drawable.ic_comment_white,
-                        contentDescription = "Comment"
+                        contentDescription = "Comment",
+                        modifier = Modifier.size(32.dp)
                     )
                 }
             )
             Tab(
-                selected = state == IDX_EXIF,
+                selected = pagerState.currentPage == IDX_EXIF,
                 onClick = {
-                    state = IDX_EXIF
                     coroutineScope.launch {
-                        pagerState.scrollToPage(state)
+                        pagerState.animateScrollToPage(IDX_EXIF)
                     }
                 },
                 icon = {
                     AsyncImage(
                         model = R.drawable.ic_tune,
-                        contentDescription = "Exif"
+                        contentDescription = "Exif",
+                        modifier = Modifier.size(32.dp)
                     )
                 }
             )
--- MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/controls/photorating/PhotoRatingScreen.kt
+++ MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/controls/photorating/PhotoRatingScreen.kt
@@ -1,15 +1,16 @@
 package us.mikeandwan.photos.ui.controls.photorating
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
@@ -32,20 +33,27 @@ fun PhotoRatingScreen(
     val avgRating = viewModel.averageRating.collectAsState()
 
     Column(modifier = Modifier.fillMaxHeight()) {
-        Row(modifier = Modifier
-            .align(Alignment.CenterHorizontally)
-            .fillMaxWidth()
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp, 16.dp, 12.dp, 0.dp)
         ) {
-            Text(text = stringResource(id = R.string.frg_rating_your_rating))
+            Text(
+                text = stringResource(id = R.string.frg_rating_your_rating),
+                color = MaterialTheme.colorScheme.onSurface
+            )
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
             RatingBar(
                 itemSize = 32.dp,
                 space = 2.dp,
                 rating = userRating.value.toFloat(),
                 imageVectorEmpty = ImageVector.vectorResource(R.drawable.ic_baseline_star_outline),
                 imageVectorFilled = ImageVector.vectorResource(R.drawable.ic_star),
-                tintEmpty = MaterialTheme.colorScheme.onPrimary,
+                tintEmpty = MaterialTheme.colorScheme.primary,
                 tintFilled = MaterialTheme.colorScheme.primary,
                 rateChangeStrategy = RateChangeStrategy.InstantChange,
                 ratingInterval = RatingInterval.Full,
@@ -57,17 +65,23 @@ fun PhotoRatingScreen(
         }
 
         Row(modifier = Modifier
-            .align(Alignment.CenterHorizontally)
             .fillMaxWidth()
+            .padding(12.dp, 16.dp, 12.dp, 0.dp)
         ) {
-            Text(text = stringResource(id = R.string.frg_rating_avg_rating))
+            Text(
+                text = stringResource(id = R.string.frg_rating_avg_rating),
+                color = MaterialTheme.colorScheme.onSurface
+            )
         }
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
             RatingBar(
                 rating = avgRating.value,
                 imageVectorEmpty = ImageVector.vectorResource(R.drawable.ic_baseline_star_outline),
                 imageVectorFilled = ImageVector.vectorResource(R.drawable.ic_star),
-                tintEmpty = MaterialTheme.colorScheme.onPrimary,
+                tintEmpty = MaterialTheme.colorScheme.primary,
                 tintFilled = MaterialTheme.colorScheme.primary,
                 gestureStrategy = GestureStrategy.None
             ) {
