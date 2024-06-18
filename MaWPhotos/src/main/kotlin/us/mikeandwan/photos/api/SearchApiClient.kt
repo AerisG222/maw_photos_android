b46b816dc2d8a31fe4aab5e4a4af10ae3d3fc2cb
Mike Morano <mmorano@mikeandwan.us>
2024-06-18 12:08:56 -0400

    now that we support video, don't exclude these from search results

--- MaWPhotos/src/main/kotlin/us/mikeandwan/photos/api/SearchApiClient.kt
+++ MaWPhotos/src/main/kotlin/us/mikeandwan/photos/api/SearchApiClient.kt
@@ -9,9 +9,6 @@ class SearchApiClient @Inject constructor(
     private val _searchApi: SearchApi by lazy { retrofit.create(SearchApi::class.java) }
 
     suspend fun searchCategories(query: String, start: Int = 0): ApiResult<SearchResults<SearchResultCategory>> {
-        // limit searches to just photos for now
-        val search = "-video $query"
-
-        return makeApiCall(::searchCategories.name, suspend { _searchApi.searchCategories(search, start) })
+        return makeApiCall(::searchCategories.name, suspend { _searchApi.searchCategories(query, start) })
     }
 }
