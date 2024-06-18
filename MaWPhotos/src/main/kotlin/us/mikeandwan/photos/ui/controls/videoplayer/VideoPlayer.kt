91cdd38291a4d0838abef2098cb8e09c92841182
Mike Morano <mmorano@mikeandwan.us>
2024-06-18 16:41:03 -0400

    pause playback when leaving app

--- MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/controls/videoplayer/VideoPlayer.kt
+++ MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/controls/videoplayer/VideoPlayer.kt
@@ -1,11 +1,14 @@
 package us.mikeandwan.photos.ui.controls.videoplayer
 
-import android.view.ViewGroup
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
@@ -36,14 +39,34 @@ fun VideoPlayer(activeMedia: Media) {
         onDispose { exoPlayer.release() }
     }
 
+    // credit: https://medium.com/@munbonecci/how-to-display-videos-using-exoplayer-on-android-with-jetpack-compose-1fb4d57778f4
+    // Add a lifecycle observer to manage player state based on lifecycle events
+    LocalLifecycleOwner.current.lifecycle.addObserver(object : LifecycleEventObserver {
+        override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+            when (event) {
+                Lifecycle.Event.ON_START -> {
+                    // Start playing when the Composable is in the foreground
+                    if (exoPlayer.isPlaying.not()) {
+                        exoPlayer.play()
+                    }
+                }
+
+                Lifecycle.Event.ON_STOP -> {
+                    // Pause the player when the Composable is in the background
+                    exoPlayer.pause()
+                }
+
+                else -> {
+                    // Nothing
+                }
+            }
+        }
+    })
+
     AndroidView(
         factory = {
             PlayerView(context).apply {
                 player = exoPlayer
-                layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
             }
         }
     )
