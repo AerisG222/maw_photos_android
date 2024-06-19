5ad84dafdf7f000d6c39a733821c25a0e3a3b9fa
Mike Morano <mmorano@mikeandwan.us>
2024-06-19 09:03:44 -0400

    improve display of bottom and top bar

--- MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/controls/mediapager/ButtonBar.kt
+++ MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/controls/mediapager/ButtonBar.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -25,11 +27,12 @@ fun ButtonBar(
     onViewDetails: () -> Unit
 ) {
     val slideshowIcon = if(isSlideshowPlaying) R.drawable.ic_stop else R.drawable.ic_play_arrow
+    val color = MaterialTheme.colorScheme.onSurface
 
     Row(
         horizontalArrangement = Arrangement.Absolute.SpaceAround,
         modifier = Modifier
-            .padding(4.dp, 2.dp)
+            .padding(2.dp, 8.dp, 2.dp, 8.dp)
             .fillMaxWidth()
     ) {
         if(activeMediaType == MediaType.Photo) {
@@ -37,7 +40,8 @@ fun ButtonBar(
                 AsyncImage(
                     model = R.drawable.ic_rotate_left,
                     contentDescription = stringResource(id = R.string.rotate_left_icon_description),
-                    modifier = Modifier.size(48.dp)
+                    modifier = Modifier.size(48.dp),
+                    colorFilter = ColorFilter.tint(color)
                 )
             }
 
@@ -45,7 +49,8 @@ fun ButtonBar(
                 AsyncImage(
                     model = R.drawable.ic_rotate_right,
                     contentDescription = stringResource(id = R.string.rotate_right_icon_description),
-                    modifier = Modifier.size(48.dp)
+                    modifier = Modifier.size(48.dp),
+                    colorFilter = ColorFilter.tint(color)
                 )
             }
 
@@ -53,7 +58,8 @@ fun ButtonBar(
                 AsyncImage(
                     model = slideshowIcon,
                     contentDescription = stringResource(id = R.string.toggle_slideshow_icon_description),
-                    modifier = Modifier.size(48.dp)
+                    modifier = Modifier.size(48.dp),
+                    colorFilter = ColorFilter.tint(color)
                 )
             }
 
@@ -61,7 +67,8 @@ fun ButtonBar(
                 AsyncImage(
                     model = R.drawable.ic_share,
                     contentDescription = stringResource(id = R.string.toggle_slideshow_icon_description),
-                    modifier = Modifier.size(48.dp)
+                    modifier = Modifier.size(48.dp),
+                    colorFilter = ColorFilter.tint(color)
                 )
             }
         }
@@ -70,7 +77,8 @@ fun ButtonBar(
             AsyncImage(
                 model = R.drawable.ic_keyboard_double_arrow_up,
                 contentDescription = stringResource(id = R.string.view_photo_details_icon_description),
-                modifier = Modifier.size(48.dp)
+                modifier = Modifier.size(48.dp),
+                colorFilter = ColorFilter.tint(color)
             )
         }
     }
--- MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/controls/scaffolds/ItemPagerScaffold.kt
+++ MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/controls/scaffolds/ItemPagerScaffold.kt
@@ -1,5 +1,6 @@
 package us.mikeandwan.photos.ui.controls.scaffolds
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,7 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -23,13 +24,17 @@ fun ItemPagerScaffold(
 ) {
     content()
 
+    val bgColor = MaterialTheme.colorScheme.surfaceContainer.copy(alpha = 0.76f)
+
     Column(
         verticalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier.fillMaxSize()
     ) {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = bgColor)
         ) {
             topLeftContent()
             Spacer(Modifier.weight(1f))
@@ -37,9 +42,9 @@ fun ItemPagerScaffold(
         }
 
         Row(modifier = Modifier
-            .height(40.dp)
+            .height(48.dp)
             .fillMaxWidth()
-            .padding(2.dp, 4.dp)
+            .background(color = bgColor)
         ) {
             bottomBarContent()
         }
