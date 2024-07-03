1d095e3057f894018728de4c7ddbf4e2135146f4
Mike Morano <mmorano@mikeandwan.us>
2024-07-03 08:26:25 -0400

    make authenticator more robust

--- MaWPhotos/src/main/kotlin/us/mikeandwan/photos/authorization/AuthAuthenticator.kt
+++ MaWPhotos/src/main/kotlin/us/mikeandwan/photos/authorization/AuthAuthenticator.kt
@@ -1,5 +1,9 @@
 package us.mikeandwan.photos.authorization
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import net.openid.appauth.AuthorizationException
 import net.openid.appauth.AuthorizationService
 import okhttp3.Authenticator
@@ -10,32 +14,59 @@ import timber.log.Timber
 import us.mikeandwan.photos.domain.AuthorizationRepository
 import java.io.IOException
 
+// https://www.coinbase.com/blog/okhttp-and-oauth-token-refreshes
 @Suppress("UNUSED_ANONYMOUS_PARAMETER")
 class AuthAuthenticator(
     private val authService: AuthorizationService,
     private val authorizationRepository: AuthorizationRepository
 ) : Authenticator {
-    @Synchronized
     @Throws(IOException::class)
     override fun authenticate(route: Route?, response: Response): Request? {
         val authState = authorizationRepository.authState.value
+        val currToken = authState.accessToken
         var request: Request? = null
+
+        if(currToken == null || authState.refreshToken == null) {
+            Timber.i("tokens are null, will not try to refresh")
+            return request
+        }
 
         Timber.d("Starting Authenticator.authenticate")
 
-        authState.performActionWithFreshTokens(authService) {
-            accessToken: String?,
-            idToken: String?,
-            ex: AuthorizationException? ->
+        synchronized(this) {
+            val potentiallyNewToken = authorizationRepository.authState.value.accessToken
 
-            when {
-                ex != null -> Timber.e(ex, "Failed to authorize")
-                accessToken == null -> Timber.e("Failed to authorize, received null access token")
-                else -> {
-                    Timber.i("authenticate: obtained access token")
-                    request = response.request.newBuilder()
-                        .header("Authorization", "Bearer $accessToken")
+            // see if prior attempt included an auth token
+            if (response.request.header("Authorization") != null) {
+                // see if token was already refreshed, and if so try that
+                if(potentiallyNewToken != currToken) {
+                    return response.request.newBuilder()
+                        .header("Authorization", "Bearer $potentiallyNewToken")
                         .build()
+                }
+            }
+
+            authState.performActionWithFreshTokens(authService) {
+                accessToken: String?,
+                idToken: String?,
+                ex: AuthorizationException? ->
+
+                when {
+                    ex != null -> Timber.e(ex, "Failed to authorize")
+                    accessToken == null -> Timber.e("Failed to authorize, received null access token")
+                    else -> {
+                        Timber.i("authenticate: obtained access token")
+
+                        runBlocking {
+                            CoroutineScope(Dispatchers.IO).launch {
+                                authorizationRepository.save(authState)
+                            }
+                        }
+
+                        request = response.request.newBuilder()
+                            .header("Authorization", "Bearer $accessToken")
+                            .build()
+                    }
                 }
             }
         }
