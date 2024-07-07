e00e4af15e9a0eddcee2ba9c21bb2863c3548053
Mike Morano <mmorano@mikeandwan.us>
2024-07-07 17:26:35 -0400

    add retries to try and cope w/ (mostly) reauth scenarios

--- MaWPhotos/src/main/kotlin/us/mikeandwan/photos/api/BaseApiClient.kt
+++ MaWPhotos/src/main/kotlin/us/mikeandwan/photos/api/BaseApiClient.kt
@@ -1,6 +1,7 @@
 package us.mikeandwan.photos.api
 
 import android.webkit.MimeTypeMap
+import kotlinx.coroutines.delay
 import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
@@ -8,25 +9,50 @@ import timber.log.Timber
 import java.io.File
 
 abstract class BaseApiClient {
+    companion object {
+        private const val MAX_ATTEMPTS = 3
+    }
+
     private val mimeMap by lazy { MimeTypeMap.getSingleton() }
 
     protected suspend fun <T> makeApiCall(name: String, apiCall: suspend () -> retrofit2.Response<T>): ApiResult<T> {
-        try {
-            val response = apiCall()
-            val result = ApiResult.build(response)
+        var attempt = 1
+        var exception: Throwable? = null
 
-            when(result) {
-                is ApiResult.Error -> Timber.w("$name failed: ${result.error}")
-                is ApiResult.Success -> Timber.d("$name succeeded")
-                is ApiResult.Empty -> Timber.d("$name was empty")
+        while (attempt <= MAX_ATTEMPTS) {
+            exception = null
+
+            if(attempt > 1) {
+                delay(200L * attempt)
             }
 
-            return result
-        } catch (t: Throwable) {
-            Timber.e(t)
+            try {
+                val response = apiCall()
+                val result = ApiResult.build(response)
 
-            return ApiResult.Error("$name failed: ${t.message}", null, t)
+                if(result is ApiResult.Error) {
+                    Timber.w("$name failed: ${result.error} (attempt: $attempt)")
+
+                    attempt++
+                    continue
+                } else {
+                    when (result) {
+                        is ApiResult.Success -> Timber.d("$name succeeded")
+                        is ApiResult.Empty -> Timber.d("$name was empty")
+                        is ApiResult.Error -> Timber.w("should not happen!")
+                    }
+
+                    return result
+                }
+            } catch (t: Throwable) {
+                exception = t
+                Timber.e("$name failed: ${t.message} (attempt: $attempt)", t)
+
+                attempt++
+            }
         }
+
+        return ApiResult.Error("$name failed", null, exception)
     }
 
     protected fun getMediaTypeForFile(file: File): MediaType {
