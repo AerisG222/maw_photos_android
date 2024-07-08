ad009a380d9dab9fc1deeb77d802b3b6dee9801e
Mike Morano <mmorano@mikeandwan.us>
2024-07-08 17:30:38 -0400

    bump version for google play

--- MaWPhotos/build.gradle.kts
+++ MaWPhotos/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "us.mikeandwan.pictures"
         minSdk = 26
         targetSdk = 34
-        versionCode = 62
+        versionCode = 64
         versionName = "8.0"
 
         manifestPlaceholders["appAuthRedirectScheme"] = "us.mikeandwan.photos"
